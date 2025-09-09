d73dc38113e97a2892e3a89b2ff47d5d962cff20
Juan Maldini <juan.maldini@outlook.com>
2025-09-09 16:55:55 -0300

    Update landing page text and button styles
    Changed the landing page title to 'Clothfigurator' and updated the button text to 'START DEMO' in App.tsx. Modified .sp-btn styles in Sidepanel.css to use white text and added a transition for all properties.

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="landing-root centered-container">
       <div className="centered-content">
-        <h1 className="landing-title">Demo</h1>
+        <h1 className="landing-title">Clothfigurator</h1>
         <Link to="/ConfiguratorSystem" className="sp-btn landing-link">
-          <div className="noUnderline">Configurator System</div>
+          <div className="noUnderline">START DEMO</div>
           <img src="../icons/Logo.png" alt="Arrow Right" className="landing-icon" />
         </Link>
       </div>
