d02a2024f57e78acbfa645df961667405108d882
Juan Maldini <juan.maldini@outlook.com>
2025-09-10 16:39:23 -0300

    Update landing page branding and button text
    Changed the main title from 'Clothfigurator' to 'Mayer Fabrics' and updated the button text from 'START DEMO' to 'START APP' for improved clarity and branding.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="landing-root centered-container">
       <div className="centered-content">
-        <h1 className="landing-title">Clothfigurator</h1>
+        <h1 className="landing-title">Mayer Fabrics</h1>
         <Link to="/mayerfabrics" className="sp-btn landing-link">
-          <div className="noUnderline">START DEMO</div>
+          <div className="noUnderline">START APP</div>
           <img src="../icons/Logo.png" alt="Arrow Right" className="landing-icon" />
         </Link>
       </div>
