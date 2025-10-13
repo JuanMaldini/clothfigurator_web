f182e61fd6c87d0c5065bef9875249c5f724cea6
Juan Maldini <juan.maldini@outlook.com>
2025-10-13 16:33:38 -0300

    Revert "Add GitHub Pages mode to Vite config"
    This reverts commit d4fed9c7c50313228bf8e3a12310a639ffb3c54b.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Use a special mode for GitHub Pages so Vercel remains unaffected
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: mode === "gh" ? "/clothfigurator_web/" : "/",
-}));
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()]
+});
