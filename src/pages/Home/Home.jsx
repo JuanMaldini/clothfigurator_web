df5f1c489d789e1656669ae2c1e61948dbf56a8e
JuanMaldini <juan.maldini@outlook.com>
2025-10-15 14:28:29 -0300

    Fix indentation in Home page component
    Corrected indentation in the Home.jsx file for improved code readability and consistency.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
 import "./Home.css";
 const Home = () => (
   <section className="home-page">
-        <h2>Home</h2>
-        <p>Welcome to the Clothfigurator home page!</p>
+    <h2>Home</h2>
+    <p>Welcome to the Clothfigurator home page!</p>
   </section>
 );
 
