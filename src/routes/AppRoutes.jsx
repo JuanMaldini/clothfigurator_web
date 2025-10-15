365faad9d425ba2207cf95069dfee96a4938d0d0
JuanMaldini <juan.maldini@outlook.com>
2025-10-15 14:34:32 -0300

    Update styles and fix Projects import case
    Removed background and border styles from Footer and Navbar components for a cleaner look. Adjusted background colors in App.css for improved contrast. Fixed incorrect import case for Projects component in AppRoutes.jsx.

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home.jsx";
-import Projects from "../pages/Projects/projects.jsx";
+import Projects from "../pages/Projects/Projects.jsx";
 import UsersPage from "../pages/users/users.jsx";
 import ControlPanel from "../pages/controlpanel/controlpanel.jsx";
 
